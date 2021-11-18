b59e6278cb12cfc3534e664144b409c34bf44cd5
EltonFernandes01 <56556548+EltonFernandes01@users.noreply.github.com>
2021-11-18 13:55:35 -0300

    Adicionar arquivos de projeto.

---
+++ AplicacaoSpecFlowBDD/Hooks/Hooks.cs
@@ -0,0 +1,33 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace SpecFlowSeleniumWebDriver.Hooks
+{
+    [Binding]
+    public sealed class Hooks
+    {
+        private WebDriver driver;        
+        private const string Username = "elton@cit.com";
+        private const string Password = "123456";
+        
+        [BeforeScenario]
+        public void CleanDatabase()
+        {
+            var chromeOptions = new ChromeOptions();
+            chromeOptions.AddArgument("--headless");
+
+            driver = new ChromeDriver(chromeOptions);
+            driver.Navigate().GoToUrl("https://seubarriga.wcaquino.me/");
+            driver.FindElement(By.Id("email")).SendKeys(Username);
+            driver.FindElement(By.Id("senha")).SendKeys(Password);
+            driver.FindElement(By.TagName("button")).Click();
+            driver.FindElement(By.LinkText("reset")).Click();
+            driver.Quit();
+        }       
+    }
+}
---
+++ AplicacaoSpecFlowBDD/LeiaMe.md
@@ -0,0 +1,20 @@
+﻿/*
+ * 
+ * Pacote de instalação 
+ * 
+  - DotNetSeleniumExtras.PageObjects
+  - DotNetSeleniumExtras.WaitHelpers
+  - Gherkin 
+  - NUnit
+  - NUnit.ConsoleRunner
+  - NUnit3TestAdapter
+  - Selenium.Support
+  - Selenium.WebDriver
+  - Selenium.WebDriver.ChromeDriver
+  - SpecFlow
+  - SpecFlow.AssistDynamic
+  - SpecFlow.Nunit
+  - SpecFlow.Tools.MsBuild.Generation
+ * 
+ * 
+ * */
