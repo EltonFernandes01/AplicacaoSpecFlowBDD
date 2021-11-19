73bd9add0355e8621629c33a480d891e22cc53c3
EltonFernandes01 <56556548+EltonFernandes01@users.noreply.github.com>
2021-11-19 14:26:13 -0300

    hooks

--- AplicacaoSpecFlowBDD/Hooks-bkp/Hooks.cs
+++ AplicacaoSpecFlowBDD/Hooks-bkp/Hooks.cs
@@ -1,4 +1,4 @@
-﻿using OpenQA.Selenium;
+﻿/*using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System;
 using System.Collections.Generic;
@@ -31,3 +31,4 @@ namespace SpecFlowSeleniumWebDriver.Hooks
         }       
     }
 }
+*/
